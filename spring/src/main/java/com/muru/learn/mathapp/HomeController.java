bedc7ed9ff4c52b2ce7b2e844abe144647021c45
Murugavel <Murugavel_Prabakaran@intuit.com>
2018-10-28 19:02:33 -0700

    Model binding

--- spring/src/main/java/com/muru/learn/mathapp/HomeController.java
+++ spring/src/main/java/com/muru/learn/mathapp/HomeController.java
@@ -1,12 +1,14 @@
 package com.muru.learn.mathapp;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/home")
-    public String hello() {
+    public String hello(Model model) {
+        model.addAttribute("name", "Spring Boot");
         return "index";
     }
 }
